1dd6080a4f108473b48d358075140a8ab977a2d6
Victor <victor.carvalhoad@gmail.com>
2025-09-17 23:47:40 -0300

    Add files via upload
    Adição portifólio web

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function enviarMsg(event){
+    event.preventDefault();
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const mensagem = document.getElementById('mensagem').value;
+
+    const texto = `Olá, me chamo ${nome} e esse é meu e-mail ${email}. ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+
+    const url = `http://wa.me/5521995114401?text=${msgFormatada}`;
+
+    window.open(url, '_blank');
+}
